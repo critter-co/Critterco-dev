d25a5048ebeb96650f13fc0d1cb0121d68ec3cb2
Hossein <mirjalali.hosein@gmail.com>
2020-05-02 13:24:43 -0400

    Added JTW authentication, only authenticated users can request api.

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework_jwt.views import obtain_jwt_token
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('biz.urls')),
+    path("admin/", admin.site.urls),
+    path("", include("biz.urls")),
+    path("token-auth/", obtain_jwt_token),
 ]
