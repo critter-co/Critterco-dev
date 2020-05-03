1f310a0c2938638f679ca6f9adf79134e9db6396
Hossein <mirjalali.hosein@gmail.com>
2020-05-03 13:50:26 -0400

    Added a new user endpoint and /me for edit, etc.

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,10 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework_jwt.views import obtain_jwt_token
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("biz.urls")),
-    path("token-auth/", obtain_jwt_token),
     path("api/user/", include("user.urls")),
 ]
