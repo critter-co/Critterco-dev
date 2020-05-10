2eed63bf4b51131fbcf2c9130d18854865a0e9af
Hossein <mirjalali.hosein@gmail.com>
2020-05-10 01:57:42 -0400

    Added "id" to read only fields in BizSerializer.

--- backend/biz/serializers.py
+++ backend/biz/serializers.py
@@ -6,10 +6,14 @@ class BizSerializer(serializers.ModelSerializer):
     class Meta:
         model = Biz
         fields = "__all__"
-        read_only_fields = ["created", "created_by"]
+        read_only_fields = ["id", "created", "created_by"]
 
 
 class HoursSerializer(serializers.ModelSerializer):
     class Meta:
         model = Hours
         fields = "__all__"
+        read_only_fields = [
+            "id",
+        ]
+
