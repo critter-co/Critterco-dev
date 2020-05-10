4176b47ac71285af3bf5b5f1a95fbd5f9474c636
Hossein <mirjalali.hosein@gmail.com>
2020-05-10 03:29:46 -0400

    read only

--- backend/user/serializers.py
+++ backend/user/serializers.py
@@ -14,8 +14,11 @@ class UserSerializer(serializers.ModelSerializer):
             "name",
             "last_name",
             "username",
+            "is_staff",
             "phone",
         )
+        read_only_fields = ["id", "is_staff"]
+
         extra_kwargs = {"password": {"write_only": True, "min_length": 5}}
 
     def create(self, validated_data):
