3bed5b0a65b1864cd16373d564ede841ee9cbcbc
Hossein <mirjalali.hosein@gmail.com>
2020-05-14 13:34:09 -0400

    Changed permission for user create endpoint to AllowAny.

--- backend/user/views.py
+++ backend/user/views.py
@@ -9,6 +9,7 @@ class CreateUserView(generics.CreateAPIView):
     """Create a new user in the system"""
 
     serializer_class = UserSerializer
+    permission_classes = (permissions.AllowAny,)
 
 
 class CreateTokenView(ObtainAuthToken):
