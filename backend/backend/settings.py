bfbfbe797f148abb21c0effa6405a65461e11659
hossein <mirjalali.hosein@gmail.com>
2020-05-17 08:11:24 -0400

    Added base throttling codes and edited comments model.

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -67,6 +67,11 @@ REST_FRAMEWORK = {
         "rest_framework.authentication.SessionAuthentication",
         "rest_framework.authentication.BasicAuthentication",
     ),
+    "DEFAULT_THROTTLE_CLASSES": [
+        "rest_framework.throttling.AnonRateThrottle",
+        "rest_framework.throttling.UserRateThrottle",
+    ],
+    "DEFAULT_THROTTLE_RATES": {"anon": "100/day", "user": "1000/day"},
 }
 
 ROOT_URLCONF = "backend.urls"
--- backend/comments/models.py
+++ backend/comments/models.py
@@ -5,8 +5,10 @@ from core.models import User
 
 class Comment(models.Model):
     content = models.TextField()
-    biz = models.ForeignKey(Biz, null=True, on_delete=models.SET_NULL)
-    user = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
+    biz = models.ForeignKey(
+        Biz, null=True, on_delete=models.CASCADE, related_name="comments"
+    )
+    user = models.ForeignKey(User, null=True, on_delete=models.CASCADE)
     reply = models.ForeignKey(
         "Comment", null=True, related_name="replies", on_delete=models.CASCADE
     )
