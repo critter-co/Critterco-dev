9d5736cee3970e4c8b699fa90b0410aa60adb20c
Critter <mirjalali.hosein@gmail.com>
2020-05-25 22:39:37 +0430

    Removed Biz field from comment serializer.

--- backend/comments/serializers.py
+++ backend/comments/serializers.py
@@ -6,4 +6,8 @@ class CommentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Comment
         fields = "__all__"
-        read_only_fields = ("id", "user", "created_at", "biz")
+        read_only_fields = (
+            "id",
+            "user",
+            "created_at",
+        )
