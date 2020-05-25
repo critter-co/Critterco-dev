6673bc4c20e0c55c96e2cc002cae99fd2ee40fc3
Critter <mirjalali.hosein@gmail.com>
2020-05-25 23:23:09 +0430

    Updating biz field isn't possible once comment is created.

--- backend/comments/serializers.py
+++ backend/comments/serializers.py
@@ -3,6 +3,15 @@ from .models import Comment
 
 
 class CommentSerializer(serializers.ModelSerializer):
+    def __init__(self, *args, **kwargs):
+        """Checks for "biz" field in comment PATCH, PUT and removes it if any is provided"""
+        super().__init__(*args, **kwargs)
+        if "view" in self.context and self.context["view"].action in [
+            "update",
+            "partial_update",
+        ]:
+            self.fields.pop("biz", None)
+
     class Meta:
         model = Comment
         fields = "__all__"
