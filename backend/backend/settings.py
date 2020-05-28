d68bf169203fd029c2c4f1eca0c008afad61d269
Critter <mirjalali.hosein@gmail.com>
2020-05-28 22:21:41 +0430

    Removed BasicAuthentication from DRF settings.

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -136,7 +136,6 @@ REST_FRAMEWORK = {
     "DEFAULT_PERMISSION_CLASSES": ("rest_framework.permissions.IsAuthenticated",),
     "DEFAULT_AUTHENTICATION_CLASSES": (
         "rest_framework.authentication.SessionAuthentication",
-        "rest_framework.authentication.BasicAuthentication",
     ),
     "DEFAULT_THROTTLE_CLASSES": [
         "rest_framework.throttling.AnonRateThrottle",
