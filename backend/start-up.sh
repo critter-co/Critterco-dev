f5da11185ad4edf9065b6e4f5ad63fa27a24921c
Critter <mirjalali.hosein@gmail.com>
2020-05-29 19:56:46 +0430

    python runs from django's builtin runserver command for dev stage.

--- backend/start-up.sh
+++ backend/start-up.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-python3 manage.py migrate --no-input
-pythn3 manage.py collectstatic
+
 
 while true; do
-    # python3 manage.py runserver 0.0.0.0:8000
-    gunicorn backend.wsgi -b 0.0.0.0:8000
+    
+    python3 manage.py migrate --no-input
+    python3 manage.py collectstatic
+    python3 manage.py runserver 0.0.0.0:8000
     sleep 5s
 done
