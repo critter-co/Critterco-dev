eab244580eafed087895e695759d4dab2851cc11
Critter <mirjalali.hosein@gmail.com>
2020-05-29 23:24:17 +0430

    collectstatic no longer needs confirmation in start-up.sh script.

--- backend/start-up.sh
+++ backend/start-up.sh
@@ -5,7 +5,7 @@
 while true; do
     
     python3 manage.py migrate --no-input
-    python3 manage.py collectstatic
+    python3 manage.py collectstatic --no-input
     python3 manage.py runserver 0.0.0.0:8000
     sleep 5s
 done
