fbee99db00a98fe27d3bfa2832001af1ad62ee73
Critter <mirjalali.hosein@gmail.com>
2020-05-29 15:48:09 +0430

    configured static and media root, directories and configs.

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -119,8 +119,16 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 STATIC_URL = "/static/"
+
+STATICFILES_DIRS = (os.path.join(BASE_DIR, "core/static"),)
+# Media configs.
+
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+MEDIA_URL = "/media/"
+
 
 # Custom user model to be used by Django.
 AUTH_USER_MODEL = "core.user"
--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls import url
 from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -12,7 +13,7 @@ urlpatterns = [
         r"^api/password_reset/",
         include("django_rest_passwordreset.urls", namespace="password_reset"),
     ),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if settings.DEBUG:
     import debug_toolbar
