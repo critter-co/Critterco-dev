9903efa87823d64e7205444bcad1b0cbc9610288
Critter <mirjalali.hosein@gmail.com>
2020-06-04 18:46:56 +0430

    Removed all migration files and made new migrations instead.
    Tests successful so far.

--- backend/comments/migrations/0001_initial.py
+++ backend/comments/migrations/0001_initial.py
@@ -11,7 +11,6 @@ class Migration(migrations.Migration):
 
     dependencies = [
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
-        ('biz', '0008_auto_20200504_0823'),
     ]
 
     operations = [
@@ -21,9 +20,6 @@ class Migration(migrations.Migration):
                 ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('content', models.TextField()),
                 ('created_at', models.DateTimeField(auto_now_add=True)),
-                ('biz', models.ForeignKey(null=True, on_delete=django.db.models.deletion.SET_NULL, to='biz.Biz')),
-                ('reply', models.ForeignKey(null=True, on_delete=django.db.models.deletion.CASCADE, related_name='replies', to='comments.Comment')),
-                ('user', models.ForeignKey(null=True, on_delete=django.db.models.deletion.SET_NULL, to=settings.AUTH_USER_MODEL)),
             ],
         ),
     ]
