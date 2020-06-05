32498b593a18081d8433029a04f8cf3acae82879
Critter <mirjalali.hosein@gmail.com>
2020-06-05 21:00:49 +0430

    full test coverage of comments view codes.

--- backend/tests/comments/comment_tests.py
+++ backend/tests/comments/comment_tests.py
@@ -1,10 +1,125 @@
-# from django.urls import reverse
-# from rest_framework.test import APIClient, APITestCase
-# from django.contrib.auth import get_user_model
-# from rest_framework import status
+from django.urls import reverse
+from django.test import TestCase
+from rest_framework.test import APIClient
+from django.contrib.auth import get_user_model
+from rest_framework import status
+from comments.models import Comment
 
-# User = get_user_model()
+COMMENT_URL = reverse('comment-list')
+# COMMENT_DETAIL_URL = reverse('comment-detail', args=comment.pk)
+CREATE_USER_URL = reverse('user:create')
+TOKEN_URL = reverse('token_obtain_pair')
+User = get_user_model()
 
 
-# class TestPatchComments(APITestCase, APIClient):
-#     """Tests for comment functions"""
+def create_user(**params):
+    return get_user_model().objects.create_user(**params)
+
+
+class TestPatchComments(TestCase):
+    """Tests for comment functions"""
+
+    def setUp(self):
+        self.client = APIClient()
+
+    def test_comment_notallowed_with_session_login(self):
+        """Test that posting comments with session login is not allowed."""
+        payload = {
+            'content': 'Test 1'
+        }
+        create_user(email="foo@test.com", password="testpassword")
+        self.client.login(email="foo@test.com", password="testpassword")
+        res = self.client.post(COMMENT_URL, payload)
+        self.assertEqual(res.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_create_comment_valid_token(self):
+        """Test that a user with valid token can post comments."""
+        payload_user = {
+            'email': 'foo@test.com',
+            'password': 'testpassword',
+            'name': 'bar'
+        }
+        res = self.client.post(CREATE_USER_URL, payload_user)
+        get_token = self.client.post(TOKEN_URL, payload_user, format='json')
+        token = get_token.data['access']
+        self.client.credentials(HTTP_AUTHORIZATION='Bearer ' + token)
+        payload = {
+            'content': 'Test 1'
+        }
+        res = self.client.post(COMMENT_URL, payload)
+        self.assertEqual(res.status_code, status.HTTP_201_CREATED)
+
+    def test_created_comment_has_user_id(self):
+        """Test that when user creates comment their user ID is in 'user' field of comment"""
+        payload_user = {
+            'email': 'foo@test.com',
+            'password': 'testpassword',
+            'name': 'bar'
+        }
+        res = self.client.post(CREATE_USER_URL, payload_user)
+        get_token = self.client.post(TOKEN_URL, payload_user, format='json')
+        token = get_token.data['access']
+        self.client.credentials(HTTP_AUTHORIZATION='Bearer ' + token)
+        payload = {
+            'content': 'Test 1'
+        }
+        res = self.client.post(COMMENT_URL, payload)
+        user = get_user_model().objects.get(email="foo@test.com")
+        user_id = user.id
+        comment = Comment.objects.latest('id')
+        comment_id = comment.user.id
+        self.assertEqual(comment_id, user_id)
+
+    def test_comment_partial_update(self):
+        """Test that users can update(edit) their own comments after they are created"""
+        payload_user = {
+            'email': 'foo@test.com',
+            'password': 'testpassword',
+            'name': 'bar'
+        }
+        res = self.client.post(CREATE_USER_URL, payload_user)
+        get_token = self.client.post(TOKEN_URL, payload_user, format='json')
+        token = get_token.data['access']
+        self.client.credentials(HTTP_AUTHORIZATION='Bearer ' + token)
+        payload = {
+            'content': 'Test 1'
+        }
+        self.client.post(COMMENT_URL, payload)
+        comment = Comment.objects.latest('id')
+        res = self.client.patch(reverse('comment-detail', args=[comment.pk]), {'content': 'EDITED'})
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        new_comment = Comment.objects.latest('id').content
+        self.assertEqual(new_comment, 'EDITED')
+
+    def test_edit_others_comment(self):
+        """Test that users can't edit other users' comments.'"""
+        payload_user = {
+            'email': 'foo@foo.com',
+            'password': 'testpassword',
+            'name': 'foo',
+            'username': 'foo'
+        }
+        res = self.client.post(CREATE_USER_URL, payload_user)
+        get_token = self.client.post(TOKEN_URL, payload_user, format='json')
+        token = get_token.data['access']
+        self.client.credentials(HTTP_AUTHORIZATION='Bearer ' + token)
+        payload = {
+            'content': 'ORIGINAL'
+        }
+        self.client.post(COMMENT_URL, payload)
+        comment = Comment.objects.latest('id')
+        self.client.credentials()
+        payload_user2 = {
+            'email': 'bar@bar.com',
+            'password': 'testpassword',
+            'name': 'bar',
+            'username': 'bar'
+        }
+        self.client.post(CREATE_USER_URL, payload_user2)
+        get_token2 = self.client.post(TOKEN_URL, payload_user2, format='json')
+        token2 = get_token2.data['access']
+        self.client.credentials(HTTP_AUTHORIZATION='Bearer ' + token2)
+        res = self.client.patch(reverse('comment-detail', args=[comment.pk]), {'content': 'EDITED'})
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+        new_comment = Comment.objects.latest('id').content
+        self.assertEqual(new_comment, 'ORIGINAL')
