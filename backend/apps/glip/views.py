58c5367e80637a6a3edce6ba364585d219f001dd
hosermes <mirjalali.hosein@gmail.com>
2021-05-19 21:05:16 +0430

    glip entity is now serialized and works with permissions

--- backend/apps/glip/views.py
+++ backend/apps/glip/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
 
-# Create your views here.
+# Glip serializer views
+from rest_framework import viewsets
+from rest_framework_extensions.cache.mixins import CacheResponseMixin
+
+from apps.core.permissions import HasGroupPermission
+from .models import Glip
+from .serializers import GlipSerializer
+
+
+class GlipViewSet(CacheResponseMixin, viewsets.ModelViewSet):
+
+    queryset = Glip.objects.all()
+    permission_classes = [HasGroupPermission]
+    required_groups = {
+        "list": ["__all__"],
+        "create": ["member", "biz_post"],
+        "upate": ["member", "biz_edit"],
+        "partial_update": ["member", "biz_edit"],
+        "destroy": ["member", "admin"],
+    }
+
+    def partial_update(self, request, *args, **kwargs):
+        kwargs["partial"] = True
+        return self.update(request, *args, **kwargs)
+
+    serializer_class = GlipSerializer
--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -11,6 +11,7 @@ from rest_framework_simplejwt.views import (
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("apps.biz.urls")),
+    path("", include("apps.glip.urls")),
     path("", include("apps.comments.urls")),
     path("api/user/", include("apps.user.urls")),
     path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
